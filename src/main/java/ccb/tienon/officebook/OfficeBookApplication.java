ce2467729973a9f806156c465271f5b963c388e0
初阳 <1004653002@qq.com>
2022-09-27 13:38:35 +0800

    测试

--- src/main/java/ccb/tienon/officebook/OfficeBookApplication.java
+++ src/main/java/ccb/tienon/officebook/OfficeBookApplication.java
@@ -3,6 +3,13 @@ package ccb.tienon.officebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @参数 : 测试
+ * @返回值 : 测试
+ * @author : mx
+ * @Time : 2022/9/27
+ * @desc : 测试
+ */
 @SpringBootApplication
 public class OfficeBookApplication {
 
