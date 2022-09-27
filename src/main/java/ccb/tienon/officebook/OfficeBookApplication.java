5519a1a6843c09cb90b44df4df03acc341596b20
初阳 <1004653002@qq.com>
2022-09-27 13:33:39 +0800

    Initial commit

---
+++ src/main/java/ccb/tienon/officebook/OfficeBookApplication.java
@@ -0,0 +1,13 @@
+package ccb.tienon.officebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ccb/tienon/officebook/OfficeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ccb.tienon.officebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
